2f171d4019502013b9a7ed08ac30a77ca172ac32
Geraldine <geraldine.autriqueLP2@gmail.com>
2018-12-12 16:07:23 +0100

    controller

--- src/main/java/com/session3/projetfinsession3/controller/ClientController.java
+++ src/main/java/com/session3/projetfinsession3/controller/ClientController.java
@@ -5,11 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.session3.projetfinsession3.model.Client;
+import com.session3.projetfinsession3.repository.ClientRepository;
+
+
+
+
 
 
 @RestController
@@ -41,6 +48,68 @@ public class ClientController {
 		
 		
 	}
+		/**
+		 * rechercher
+		 * @param id
+		 * @return
+		 */
+		@RequestMapping(value = "/client/{id}", method = RequestMethod.GET)
+		public ResponseEntity<?> getClient(@PathVariable long id){
+			Client client = null;
+					
+			try {
+				client =ClientRepository.findOne(id);
+			} catch (Exception e) {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+			}
+			
+			if(client == null)
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+			
+			return ResponseEntity.status(HttpStatus.OK).body(client);
+		}
+		
+		/**
+		 * ajouter
+		 * @param client
+		 * @return
+		 */
+		@RequestMapping(value = "/client", method = RequestMethod.POST)
+		public ResponseEntity<?> addClient(@RequestBody Client apprenant){
+			Client resultClient = null;
+			String prenom = client.getPrenom();
+			if((prenom == null) || (prenom.isEmpty()))
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Il manque le prénom !");
+			
+			String nom = client.getNom();
+			if((nom == null) || (nom.isEmpty()))
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Il manque le nom !");
+			
+			try {
+				resultClient = ClientRepository.saveAndFlush(client);
+			} catch (Exception e) {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+			}
+			
+			return ResponseEntity.status(HttpStatus.CREATED).body(resultClient);
+		}
+		
+		/**
+		 * Détruire
+		 * @param id
+		 * @return
+		 */
+		@RequestMapping(value = "/client/{id}", method = RequestMethod.DELETE)
+		public ResponseEntity<?> deleteClient(@PathVariable long id){
+			try {
+			clientRepository.delete(id);
+			} catch (Exception e) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+			}
+			
+			return ResponseEntity.status(HttpStatus.OK).body(null);
+		}
+		
 	
 	
 
