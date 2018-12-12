ec2f4d646dae54c649bdd77a82b4d6dc6fda6d5f
SIMFOR090 <brucewi.pb@gmail.com>
2018-12-12 16:53:25 +0100

    avancement

--- src/main/java/com/session3/projetfinsession3/controller/AdresseController.java
+++ src/main/java/com/session3/projetfinsession3/controller/AdresseController.java
@@ -1,10 +1,20 @@
 package com.session3.projetfinsession3.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.session3.projetfinsession3.model.Adresse;
 import com.session3.projetfinsession3.repository.AdresseRepository;
+
+import co.simplon.springboot.apprenant.model.Apprenant;
 
 @RestController
 @RequestMapping("/api")
@@ -17,6 +27,68 @@ public class AdresseController {
 		
 	}
 	
-	@RequestMapping(value = "/adresse")
+	//rechercher toutes les adresses
 	
+	@RequestMapping(value = "/adresse", method = RequestMethod.GET)
+	public ResponseEntity<?> getAllAdresse(){
+		List<Adresse> listeAdresse = null;
+		try {
+			listeAdresse = adresseRepo.findAll();
+		}
+		catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(listeAdresse);
+	}
+	
+	//ajouter une adresse
+	
+	@RequestMapping(value = "/adresse", method = RequestMethod.GET)
+	public ResponseEntity<?> addAdresse(@RequestBody Adresse adresse){
+		Adresse resultAdresse = null;
+		String adresseNom = adresse.getAdresseNom();
+		if((adresseNom == null) || (adresseNom.isEmpty()))
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Il manque le nom de l'adresse!");
+		
+		String code_postal = adresse.getCode_postal();
+		if((nom == null) || (nom.isEmpty()))
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Il manque le code postal !");
+		
+		String ville = adresse.getVille();
+		if((ville == null) || (ville.isEmpty()))
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Il manque la ville !");
+		
+		try {
+			resultAdresse = adresseRepo.saveAndFlush(adresse);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(resultAdresse);
+	}
+	
+	//modifier une adresse
+	
+	@RequestMapping(value = "/adresse/{id_adresse}", method = RequestMethod.PUT)
+	public ResponseEntity<?> updateApprenant(@RequestBody Apprenant apprenant,@PathVariable Integer id) throws Exception {
+		Apprenant resultApprenant = null;
+		String prenom = apprenant.getPrenom();
+		if((prenom == null) || (prenom.isEmpty()))
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Il manque le prénom !");
+		
+		String nom = apprenant.getNom();
+		if((nom == null) || (nom.isEmpty()))
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Il manque le nom !");
+		
+		try {
+			resultApprenant = apprenantRepository.save(apprenant);
+			
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(resultApprenant);
+	}
 }
--- src/main/java/com/session3/projetfinsession3/model/Adresse.java
+++ src/main/java/com/session3/projetfinsession3/model/Adresse.java
@@ -31,8 +31,8 @@ public class Adresse implements Serializable{
 	private long id_adresse;
 	
 	@Size(max = 100)
-	@Column(name = "Adresse")
-	private String adresse;
+	@Column(name = "AdresseNom")
+	private String adresseNom;
 	
 	@Size(max = 5)
 	@Column(name = "Code_Postal")
@@ -46,20 +46,133 @@ public class Adresse implements Serializable{
 	@JoinColumn(name="Client_ID")
 	private Client client;
 
-	public Adresse( @Size(max = 100) String adresse, @Size(max = 5) String code_postal,
-			@Size(max = 100) String ville, Client client) {
 	
+
+	public Adresse() {
 		
-		this.adresse = adresse;
+	}
+
+
+
+	public Adresse(@Size(max = 100) String adresseNom, @Size(max = 5) String code_postal, @Size(max = 100) String ville,
+			Client client) {
+		super();
+		this.adresseNom = adresseNom;
 		this.code_postal = code_postal;
 		this.ville = ville;
 		this.client = client;
 	}
 
-	public Adresse() {
-		
+
+
+	public long getId_adresse() {
+		return id_adresse;
 	}
-	
+
+
+
+	public void setId_adresse(long id_adresse) {
+		this.id_adresse = id_adresse;
+	}
+
+
+
+	public String getAdresseNom() {
+		return adresseNom;
+	}
+
+
+
+	public void setAdresseNom(String adresseNom) {
+		this.adresseNom = adresseNom;
+	}
+
+
+
+	public String getCode_postal() {
+		return code_postal;
+	}
+
+
+
+	public void setCode_postal(String code_postal) {
+		this.code_postal = code_postal;
+	}
+
+
+
+	public String getVille() {
+		return ville;
+	}
+
+
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+
+
+	public Client getClient() {
+		return client;
+	}
+
+
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adresseNom == null) ? 0 : adresseNom.hashCode());
+		result = prime * result + ((client == null) ? 0 : client.hashCode());
+		result = prime * result + ((code_postal == null) ? 0 : code_postal.hashCode());
+		result = prime * result + (int) (id_adresse ^ (id_adresse >>> 32));
+		result = prime * result + ((ville == null) ? 0 : ville.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Adresse other = (Adresse) obj;
+		if (adresseNom == null) {
+			if (other.adresseNom != null)
+				return false;
+		} else if (!adresseNom.equals(other.adresseNom))
+			return false;
+		if (client == null) {
+			if (other.client != null)
+				return false;
+		} else if (!client.equals(other.client))
+			return false;
+		if (code_postal == null) {
+			if (other.code_postal != null)
+				return false;
+		} else if (!code_postal.equals(other.code_postal))
+			return false;
+		if (id_adresse != other.id_adresse)
+			return false;
+		if (ville == null) {
+			if (other.ville != null)
+				return false;
+		} else if (!ville.equals(other.ville))
+			return false;
+		return true;
+	}
+
 	
 
 	
